241587514c5c56241eba9943cf5c2e5074d061c0
Ashley <ashleynjeri7@gmail.com>
2022-12-10 08:09:21 +0300

    routes file updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Defines the root path route ("/")
+# root "articles#index"
+ root 'pages#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+ namespace :api do
+  namespace :vi do
+    resources :airlines, param: :slug
+    resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  get '#path', to: 'path#index', via :all
+
 end
