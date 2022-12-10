73b974d3764fc8da1769fe4b9a4072dd52f82284
Ashley <ashleynjeri7@gmail.com>
2022-12-10 07:41:25 +0300

    new airline serializer created

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,14 @@
 class Airline < ApplicationRecord
+    has_many :reviews
+
+    before_create :slugify # sets sulgify before created in db
+
+    # Converts  name to lowercase & adds hyphen to whitespaces
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.average(:score).round(2).to_f
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts 'Seeding data...'
+
+airlines = Airline.create([
+    { 
+      name: "Kenya Airways",
+      image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+    }, 
+    { 
+      name: "Fly540",
+      image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+    },
+    { 
+      name: "Jambojet",
+      image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+    }, 
+    { 
+      name: "Skyward Express",
+      image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+    }, 
+    { 
+      name: "Safarilink",
+      image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+    }, 
+    { 
+      name: "Phoenix Aviation",
+      image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+    }
+  ])
+
+reviews = Review.create([
+        {
+            title: 'Okay airline',
+            description: 'The meals were tasty.',
+            score: 5,
+            airline: airlines.first
+        },
+        {
+            title: 'Horrible experience',
+            description: 'Questionable hygiene.',
+            score: 1,
+            airline: airlines.first
+        }
+    ]
+  )
+
+
+
+puts 'Seeding done!'
